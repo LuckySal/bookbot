5d7cb0865295ebb14ea849622efc2c355631cc6e
LuckySal <hillbp2014@gmail.com>
2025-12-15 14:57:42 -0500

    implement book report functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, format_counts
 
 def get_book_text(filepath):
     with open(filepath, "r") as f:
@@ -6,11 +6,23 @@ def get_book_text(filepath):
     return data
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
-    frankenstein_length = count_words(frankenstein)
-    character_counts = count_characters(frankenstein)
-    print(f'Found {frankenstein_length} total words')
-    print(character_counts)
+    report = (
+                '============ BOOKBOT ============\n'
+                'Analyzing book found at {filepath}\n'
+                '----------- Word Count ----------\n'
+                'Found {wordcount} total words\n'
+                '--------- Character Count -------'
+            )
+    filepath = 'books/frankenstein.txt'
+
+    text = get_book_text(filepath)
+    book_length = count_words(text)
+    character_counts = format_counts(count_characters(text))
+    
+    print(report.format(filepath=filepath, wordcount=book_length))
+    for character in character_counts:
+        print(f'{character['char']}: {character['num']}')
+    print('============= END ===============')
 
 if __name__ == "__main__":
     main()
