0cb858d7b808fc4376ea9784b8c6efb72ecfdddb
LuckySal <hillbp2014@gmail.com>
2025-12-15 15:02:29 -0500

    use command line argument for filepath

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, format_counts
 
 def get_book_text(filepath):
@@ -6,23 +7,26 @@ def get_book_text(filepath):
     return data
 
 def main():
-    report = (
-                '============ BOOKBOT ============\n'
-                'Analyzing book found at {filepath}\n'
-                '----------- Word Count ----------\n'
-                'Found {wordcount} total words\n'
-                '--------- Character Count -------'
-            )
-    filepath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+    else:
+        report = (
+                    '============ BOOKBOT ============\n'
+                    'Analyzing book found at {filepath}\n'
+                    '----------- Word Count ----------\n'
+                    'Found {wordcount} total words\n'
+                    '--------- Character Count -------'
+                )
+        filepath = sys.argv[1]
 
-    text = get_book_text(filepath)
-    book_length = count_words(text)
-    character_counts = format_counts(count_characters(text))
-    
-    print(report.format(filepath=filepath, wordcount=book_length))
-    for character in character_counts:
-        print(f'{character['char']}: {character['num']}')
-    print('============= END ===============')
+        text = get_book_text(filepath)
+        book_length = count_words(text)
+        character_counts = format_counts(count_characters(text))
+
+        print(report.format(filepath=filepath, wordcount=book_length))
+        for character in character_counts:
+            print(f'{character['char']}: {character['num']}')
+        print('============= END ===============')
 
 if __name__ == "__main__":
     main()
